9782ed54ebac7aa7b3b52d5b74480510d49b50ed
Ahmad Kamal <Ahmad106107@gmail.com>
2019-07-03 14:27:20 +0200

    initializing modules

---
+++ app.js
@@ -0,0 +1,14 @@
+// Budget controller
+var budgetController = (function() {
+  // some code
+})();
+
+// UI controller
+var uIController = (function() {
+  // some code
+})();
+
+// Global App controller
+var controller = (function(budgetCtr, uICtr) {
+  // some code
+})(budgetController, uIController);
