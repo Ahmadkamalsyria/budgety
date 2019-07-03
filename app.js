9c7a66b716aaf5c449304cd8b82835f5b6d98f38
Ahmad Kamal <Ahmad106107@gmail.com>
2019-07-03 15:43:49 +0200

    getting inputs value

--- app.js
+++ app.js
@@ -5,10 +5,37 @@ var budgetController = (function() {
 
 // UI controller
 var uIController = (function() {
-  // some code
+  var domStrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn"
+  };
+  return {
+    getInputValues: function() {
+      return {
+        type: document.querySelector(domStrings.inputType).value,
+        description: document.querySelector(domStrings.inputDescription).value,
+        value: document.querySelector(domStrings.inputValue).value
+      };
+    },
+    getDOMStrings: function() {
+      return domStrings;
+    }
+  };
 })();
 
 // Global App controller
-var controller = (function(budgetCtr, uICtr) {
-  // some code
+var controller = (function(budgetCtr, UICtr) {
+  var DOM = UICtr.getDOMStrings();
+  var addItem = function() {
+    var inputs = UICtr.getInputValues();
+    console.log(inputs);
+  };
+  document.querySelector(DOM.addBtn).addEventListener("click", addItem);
+  document.addEventListener("keypress", function(e) {
+    if (e.keyCode === 13 || e.which === 13) {
+      addItem();
+    }
+  });
 })(budgetController, uIController);
