c6317a2d900f4f97de7d2f97f5665994ed7aa0f3
Ahmad Kamal <Ahmad106107@gmail.com>
2019-09-02 15:57:59 +0200

    display budget

--- app.js
+++ app.js
@@ -19,9 +19,28 @@ var budgetController = (function () {
     totals: {
       exp: 0,
       inc: 0
-    }
+    },
+    budget: 0,
+    percentage: -1
   }
+  var calculateTotals = function (type) {
+    var sum = 0;
+    data.allItems[type].forEach(item => {
+      sum += item.value;
+    })
+    data.totals[type] = sum;
+  }
+
   return {
+    getBudget: function () {
+      return {
+        percentage: data.percentage,
+        budget: data.budget,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp
+
+      }
+    },
     addNewItem: function (type, des, val) {
       var newItem, ID
       if (data.allItems[type].length > 0) {
@@ -38,7 +57,20 @@ var budgetController = (function () {
       }
       data.allItems[type].push(newItem);
       return newItem;
+    },
+    updateData: function () {
+      // calculate income and expenses
+      calculateTotals('exp');
+      calculateTotals('inc');
+      //calculate budget
+      data.budget = data.totals.inc - data.totals.exp;
+      //calculate percentage
+      if (data.totals.inc > 0) {
+        data.percentage = Math.round(data.totals.exp / data.totals.inc * 100);
+      } else
+        data.percentage = -1
     }
+
 
   }
 
@@ -54,18 +86,32 @@ var uIController = (function () {
     inputValue: ".add__value",
     addBtn: ".add__btn",
     expenseList: ".expenses__list",
-    incomeList: ".income__list"
+    incomeList: ".income__list",
+    budgetLabel: ".budget__value",
+    incomeLabel: ".budget__income--value",
+    expenseLabel: ".budget__expenses--value",
+    percentageLabel: ".budget__expenses--percentage"
+
   };
   return {
     getInputValues: function () {
       return {
         type: document.querySelector(domStrings.inputType).value,
         description: document.querySelector(domStrings.inputDescription).value,
-        value: document.querySelector(domStrings.inputValue).value
+        value: parseFloat(document.querySelector(domStrings.inputValue).value)
       };
     },
+    displayBudget: function (obj) {
+      document.querySelector(domStrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(domStrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(domStrings.expenseLabel).textContent = obj.totalExp;
+      if (obj.percentage > 0) {
+        document.querySelector(domStrings.percentageLabel).textContent = obj.percentage;
+      } else
+        document.querySelector(domStrings.percentageLabel).textContent = '--';
+    },
     addNewItemToTheDom: function (obj, type) {
-      console.log(obj);
+
       var html, newHtml, element;
       // get the html 
       if (type === 'inc') {
@@ -84,9 +130,26 @@ var uIController = (function () {
       newHtml = newHtml.replace('%value%', obj.value);
 
       // display 
-      console.log(element);
+
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
+
+    clearFields: function () {
+      // var des, val;
+      // des = document.querySelector(domStrings.inputDescription);
+      // val = document.querySelector(domStrings.inputValue);
+      // des.value = '';
+      // val.value = '';
+      var elements;
+      elements = document.querySelectorAll(domStrings.inputDescription + ', ' + domStrings.inputValue);
+      elementsArray = Array.prototype.slice.call(elements);
+      elementsArray.forEach(function (current, index, array) {
+        current.value = "";
+
+      });
+      elementsArray[0].focus();
+    },
+
     getDOMStrings: function () {
       return domStrings;
     }
@@ -97,24 +160,45 @@ var uIController = (function () {
 var controller = (function (budgetCtr, UICtr) {
   var setUpEventLestiners = function () {
     var DOM = UICtr.getDOMStrings();
+
     document.querySelector(DOM.addBtn).addEventListener("click", addItem);
     document.addEventListener("keypress", function (e) {
       if (e.keyCode === 13 || e.which === 13) {
         addItem();
       }
     });
+
+
+  }
+  var updateBudget = function () {
+    budgetCtr.updateData()
+    var data = budgetCtr.getBudget();
+    UICtr.displayBudget(data)
   }
 
   var addItem = function () {
     var inputs = UICtr.getInputValues();
-    var item = budgetCtr.addNewItem(inputs.type, inputs.description, inputs.value);
-    console.log(item);
-    UICtr.addNewItemToTheDom(item, inputs.type);
+
+    if (inputs.description !== '' && !isNaN(inputs.value)) {
+      var item = budgetCtr.addNewItem(inputs.type, inputs.description, inputs.value);
+      console.log(item);
+      UICtr.addNewItemToTheDom(item, inputs.type);
+      UICtr.clearFields()
+      updateBudget();
+    }
+
   };
 
   return {
     init: function () {
       setUpEventLestiners()
+      UICtr.displayBudget({
+        percentage: 0,
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0
+
+      })
     }
   }
 
